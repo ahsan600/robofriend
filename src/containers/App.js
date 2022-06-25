d175837afa961868dda15d51f4586dd80f23d094
ahsanameenx <>
2022-06-25 21:39:28 +0500

    robo

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,21 +3,18 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
+import {robots} from '../robots.js'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: [],
+      robots: robots,
       searchfield: ''
     }
   }
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=> response.json())
-      .then(users => {this.setState({ robots: users})});
-  }
+
 
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value })
@@ -35,8 +32,9 @@ class App extends Component {
           <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
           <Scroll>
-            <CardList robots={filteredRobots} />
+          <CardList robots={filteredRobots} />
           </Scroll>
+        
         </div>
       );
   }
